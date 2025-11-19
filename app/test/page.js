480ba320bc5ac9d19ff7c897bb4b35b3dd2a024a
kminsoo0110 <kminsoo0110@naver.com>
2025-11-19 14:46:28 +0900

    Finish initializing global.css.

---
+++ app/test/page.js
@@ -0,0 +1,9 @@
+function Test() {
+  return (
+    <div className="flex-col font-bold bg-point min-h-screen">
+      <h1 className="p-10 bg-gold-999">컴포넌트 실험 페이지</h1>
+    </div>
+  );
+}
+
+export default Test;
